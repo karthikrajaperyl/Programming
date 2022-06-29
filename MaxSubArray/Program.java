b1ba520a7dc71dd63609f04b29966f0d8f442cf9
karthikrajaperyl <77962533+karthikrajaperyl@users.noreply.github.com>
2022-06-29 16:13:54 +0530

    Update Program.java

--- MaxSubArray/Program.java
+++ MaxSubArray/Program.java
@@ -1,3 +1,4 @@
+package MaxSubArray;
 
 import java.util.*;
 
@@ -7,7 +8,7 @@ public class Program {
 		int start;
 		Scanner scannerObj = new Scanner(System.in);
 		HashMap<Integer, Integer> map = new HashMap<>();
-		int sum = 0, max = 0;
+		int sum = 0, max = 0, startIndex = 0, endIndex = 0;
 		int n = scannerObj.nextInt();
 		int[] a = new int[n];
 		map.put(0, -1);
@@ -18,11 +19,14 @@ public class Program {
 			sum += (a[j] == 0) ? -1 : 1;
 			if (map.containsKey(sum)) {
 				max = Math.max(max, j - map.get(sum));
+				startIndex = map.get(sum) + 1;
+				endIndex = j;
 			} else {
-				map.put(sum, j );
+				map.put(sum, j);
 			}
 		}
 		System.out.println(max);
+		System.out.println("Start Index- " + startIndex + "End Index- " + endIndex);
 	}
 
 }
