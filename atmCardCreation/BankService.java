4ea9fb7ca012cbcb0b6b00ee3a90aa8e9e0e25a1
karthikrajaperyl <77962533+karthikrajaperyl@users.noreply.github.com>
2022-07-07 16:24:40 +0530

    Delete atmCardCreation directory

--- atmCardCreation/BankService.java
+++
@@ -1,7 +0,0 @@
-package atmCardCreation;
-
-public interface BankService {
-	void validUser(int pin, ATMCardDetails cardInfo);
-
-	Transaction executeTransaction(Account account, ATMCard cardinfo);
-}
